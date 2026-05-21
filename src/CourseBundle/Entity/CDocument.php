0492dc3b106986d16020388581fc1033001c5d3c
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2026-05-21 17:37:09 -0500

    Security: Document: Enforce `ROLE_USER` check and restrict access to DRAFT-visibility links for non-teachers
    See advisory GHSA-rm3h-fm8x-8mjp

--- src/CourseBundle/Entity/CDocument.php
+++ src/CourseBundle/Entity/CDocument.php
@@ -236,8 +236,22 @@ use Symfony\Component\Validator\Constraints as Assert;
             deserialize: false
         ),
         new GetCollection(
+            parameters: [
+                'cid' => new QueryParameter(
+                    required: true,
+                    schema: ['type' => 'integer'],
+                    description: 'Course identifier',
+                ),
+            ],
             openapi: new Operation(
                 parameters: [
+                    new Parameter(
+                        name: 'cid',
+                        in: 'query',
+                        description: 'Course identifier',
+                        required: true,
+                        schema: ['type' => 'integer'],
+                    ),
                     new Parameter(
                         name: 'resourceNode.parent',
                         in: 'query',
@@ -247,7 +261,8 @@ use Symfony\Component\Validator\Constraints as Assert;
                     ),
                 ],
             ),
-            provider: DocumentCollectionStateProvider::class
+            security: "is_granted('ROLE_USER')",
+            provider: DocumentCollectionStateProvider::class,
         ),
         new Get(
             uriTemplate: '/documents/{cid}/usage',
